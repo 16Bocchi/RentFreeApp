b8f4b14bc05f279adbf11e0d02b44ee3e73011af
16Bocchi <oce.bocchi@gmail.com>
2023-02-15 10:29:55 +1100

    Initial Commit

---
+++ RentFreeApp/RentFreeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentFreeAppApp.swift
+//  RentFreeApp
+//
+//  Created by Daniel Braithwaite on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct RentFreeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
