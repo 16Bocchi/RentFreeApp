503faa908746f5406a52f2fab8edd9d1d02b2434
16Bocchi <oce.bocchi@gmail.com>
2023-02-15 10:49:00 +1100

    Initial Commit

---
+++ RentFreeApp/Browsing.swift
@@ -0,0 +1,20 @@
+//
+//  Browsing.swift
+//  RentFreeApp
+//
+//  Created by Daniel Braithwaite on 15/2/2023.
+//
+
+import SwiftUI
+
+struct Browsing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Browsing_Previews: PreviewProvider {
+    static var previews: some View {
+        Browsing()
+    }
+}
