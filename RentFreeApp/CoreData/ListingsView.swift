fac895e077e5a62be8eb0508050ed2c52e65e86c
16Bocchi <oce.bocchi@gmail.com>
2023-02-16 09:36:58 +1100

    Coredata

---
+++ RentFreeApp/CoreData/ListingsView.swift
@@ -0,0 +1,66 @@
+//
+//  ListingsView.swift
+//  RentFreeApp
+//
+//  Created by Daniel Braithwaite on 15/2/2023.
+//
+
+import SwiftUI
+import CoreData
+
+struct ListingsView: View {
+    @State var location: String = ""
+    @State var rating: Float = Float()
+    @State var price: Float = Float()
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(entity: Listings.entity(), sortDescriptors: [])
+    private var listings: FetchedResults<Listings>
+    
+    
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                TextField("Location", text: $location)
+                TextField("Rating", value: $rating, format: .number)
+                TextField("Price per night", value: $price, format: .number)
+                HStack {
+                    Spacer()
+                    Button("Add") {
+                        
+                    }
+                    Spacer()
+                    Button("Clear") {
+                        location = ""
+                        //                    rating
+                        //                    price
+                    }
+                    Spacer()
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                List{
+                    ForEach(listings){listings in
+                        HStack{
+                            Text(listings.location ?? "not found")
+                            Spacer()
+                            Text(listings.rating ?? "not found")
+                            //                        Spacer()
+                            //                        Text(listings.price ?? "not found")
+                        }
+                    }
+                }
+                .navigationTitle("Listings")
+            }
+        }
+    }
+}
+    
+struct ListingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListingsView()
+    }
+}
---
+++ RentFreeApp/CoreData/PersistenceController.swift
@@ -0,0 +1,24 @@
+//
+//  CoreDataStack.swift
+//  RentFreeApp
+//
+//  Created by Daniel Braithwaite on 15/2/2023.
+//
+
+import Foundation
+import CoreData
+
+struct PersistenceController{
+    static let shared = PersistenceController()
+    let container: NSPersistentContainer
+    
+    init(){
+        container = NSPersistentContainer(name: "Listings")
+        
+        container.loadPersistentStores{(storeDescription, error) in
+            if let error = error as NSError?{
+                fatalError("Container load failed: \(error)")
+            }
+        }
+    }
+}
