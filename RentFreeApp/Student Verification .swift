3af9481b4fc7e57180715bf3b63fa134cb91e231
chamoonoo <chamoonoo@gmail.com>
2023-02-17 10:33:50 +1100

    Fixed Student Verification page

--- RentFreeApp/Student Verification .swift
+++ RentFreeApp/Student Verification .swift
@@ -11,29 +11,29 @@ struct Student_Verification_: View {
     var body: some View {
         //17 Feb, 9:43 removed multiple VStacks
         VStack {
-
-                                    Text("Verify your Student ID")
-                                        .font(.largeTitle)
-                                        .fontWeight(.bold)
-                                
-                                        Form {
-                                            Section {
-                                                TextField("First Name"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
-                                                
-                                                TextField("Last Name"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
-                                            }
-                                            Section {
-                                                TextField("Country"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
-                                                TextField("Name of University / College"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
-                                                TextField("Student Email"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
-                                                TextField("Expected Graduation Date DD/MM/YYYY"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
-                                                
-                                            }
-                                            VStack {
-                                                Button("Verify") {
-                                                    }
-                                                }
-                                            }
+            
+            Text("Verify your Student ID")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Form {
+                Section {
+                    TextField("First Name"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
+                    
+                    TextField("Last Name"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
+                }
+                Section {
+                    TextField("Country"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
+                    TextField("Name of University / College"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
+                    TextField("Student Email"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
+                    TextField("Expected Graduation Date DD/MM/YYYY"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
+                    
+                }
+                VStack {
+                    Button("Verify") {
+                    }
+                }
+            }
         }
     }
     
@@ -43,3 +43,4 @@ struct Student_Verification_: View {
             Student_Verification_()
         }
     }
+}
