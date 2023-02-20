894ed79d36af93a1a5a01b1ddfb83147e150994a
alicesari <alicepsari@gmail.com>
2023-02-21 10:17:24 +1100

    123

---
+++ RentFreeApp/CarouselPost.swift
@@ -0,0 +1,15 @@
+//
+//  CarouselPost.swift
+//  RentFreeApp
+//
+//  Created by Alice Purnama Sari on 21/2/2023.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct Post: Identifiable{
+    var id = UUID().uuidString
+    var postImage: String
+}
